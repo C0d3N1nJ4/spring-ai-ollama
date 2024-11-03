842d9180a8983fb164dc46ec95055996ab0d652c
Naiomi <naiomi.naidoo@icloud.com>
2024-11-03 14:58:55 +0100

    Initial project commit

---
+++ backend/backend/src/test/java/com/naiomi/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naiomi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
